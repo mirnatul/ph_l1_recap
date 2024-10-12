0ec53a23f488b5ea0d4d83f60806a0ab11cbe264
mirnatul <www.mirnatul@gmail.com>
2024-10-12 23:24:02 +0600

    module 19 completed

---
+++ milestone-4/ternary.js
@@ -0,0 +1,11 @@
+const age = 12;
+
+// if (age >= 18) {
+//     console.log("You can vote");
+// }
+// else {
+//     console.log("Grow up...");
+// }
+
+// condition ? true value : false value
+age >= 18 ? console.log("You can vote") : console.log("Grow up");
